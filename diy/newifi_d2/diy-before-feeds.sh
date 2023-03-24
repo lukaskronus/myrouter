5f915fd37f406f28926919a260669beb02d72052
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-03-25 06:58:47 +0700

    Update code

--- diy/newifi_d2/diy-before-feeds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Helloworld packages collection
-git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
-# LuCi for dnsproxy
-git clone --depth=1 https://github.com/NagaseKouichi/luci-app-dnsproxy.git package/luci-app-dnsproxy
-# LuCi for xray-core
-git clone --depth=1 -b luci2 https://github.com/bi7prk/luci-app-xray.git package/luci-app-xray
