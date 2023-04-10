34dbd3efa63af54109c6460aa2dfabc15f9c106e
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-04-10 22:23:20 +0700

    Fix link not found

--- diy/03_mt7621.sh
+++ diy/03_mt7621.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Match Vermagic
-latest_release="$(curl -s https://github.com/immortalwrt/immortalwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
+latest_version="$(curl -s https://github.com/immortalwrt/immortalwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
 wget https://downloads.immortalwrt.org/releases/${latest_release}/targets/ramips/mt7621/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
