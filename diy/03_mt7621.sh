92ace56e7325375f1b0e495773dd0a7bdda8d0e5
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2023-04-10 14:49:22 +0700

    Update 03_mt7621.sh

--- diy/03_mt7621.sh
+++ diy/03_mt7621.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Match Vermagic
-latest_release="$(curl -s https://api.github.com/repos/immortalwrt/immortalwrt/tags | grep -Eo "v21.02.+[0-9\.]" | head -n 1)"
-wget https://downloads.immortalwrt.org/releases/${latest_release}/targets/ramips/mt7621/packages/Packages.gz
+wget https://downloads.immortalwrt.org/releases/21.02.5/targets/ramips/mt7621/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' > .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
