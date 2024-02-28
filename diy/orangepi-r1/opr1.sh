3c6e62a63de2aa2aeba0f2f42e3f230fb55eefe5
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2024-02-28 15:37:40 +0700

    Update Orange Pi R1 to 22.03.6

--- diy/orangepi-r1/opr1.sh
+++ diy/orangepi-r1/opr1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export OP_BUILD_PATH=$PWD
+
 # Clone source code
-git clone --single-branch --filter=blob:none -b v21.02.7 https://github.com/openwrt/openwrt openwrt_release
-git clone --single-branch --filter=blob:none -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
+git clone --single-branch --filter=blob:none -b v22.03.6 https://github.com/openwrt/openwrt openwrt_release
+git clone --single-branch --filter=blob:none -b openwrt-22.03 https://github.com/openwrt/openwrt openwrt
 
 # Clone packages
 #git clone -b master --depth 1 --filter=blob:none https://github.com/immortalwrt/immortalwrt.git immortalwrt
@@ -10,7 +12,7 @@ git clone --single-branch --filter=blob:none -b openwrt-21.02 https://github.com
 # Move stable to snapshot
 rm -f ./openwrt/include/version.mk
 rm -f ./openwrt/include/kernel.mk
-rm -f ./openwrt/include/kernel-5.4
+rm -f ./openwrt/include/kernel-5.10
 rm -f ./openwrt/include/kernel-version.mk
 rm -f ./openwrt/include/toolchain-build.mk
 rm -f ./openwrt/include/kernel-defaults.mk
@@ -19,7 +21,7 @@ rm -rf ./openwrt/target/linux/*
 rm -rf ./openwrt/package/kernel/linux/*
 cp -f ./openwrt_release/include/version.mk ./openwrt/include/version.mk
 cp -f ./openwrt_release/include/kernel.mk ./openwrt/include/kernel.mk
-cp -f ./openwrt_release/include/kernel-5.4 ./openwrt/include/kernel-5.4
+cp -f ./openwrt_release/include/kernel-5.10 ./openwrt/include/kernel-5.10
 cp -f ./openwrt_release/include/kernel-version.mk ./openwrt/include/kernel-version.mk
 cp -f ./openwrt_release/include/toolchain-build.mk ./openwrt/include/toolchain-build.mk
 cp -f ./openwrt_release/include/kernel-defaults.mk ./openwrt/include/kernel-defaults.mk
@@ -31,21 +33,24 @@ cp -rf ./openwrt_release/package/kernel/linux/* ./openwrt/package/kernel/linux/
 
 shopt -s extglob
 
+cd "${OP_BUILD_PATH}"/openwrt/
+
 ## Prepare
 git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini.git ./package/luci-app-cpu-status-mini
 
 ## Update feeds
-./scripts/feeds update -a && ./scripts/feeds install -a
+./scripts/feeds update -a 
+patch -p1 < "${OP_BUILD_PATH}"/diy/orangepi-r1/All_openwrt-22.03.6.patch
+./scripts/feeds install -a
 
 # Irqbalance
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 
 # My modificaions
-sed -i 's/192.168.1.1/192.168.41.1/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWRT/AkiKiiro/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Vermagic
-wget https://downloads.openwrt.org/releases/21.02.7/targets/ramips/mt7621/packages/Packages.gz
+wget https://downloads.openwrt.org/releases/22.03.6/targets/ramips/mt7621/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
