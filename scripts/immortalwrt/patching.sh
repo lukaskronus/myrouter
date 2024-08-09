6b5e9d1ae9dcc374d254c07bdb47b2480eb7c7fa
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2024-08-09 08:27:59 +0700

    Cleanup repo

--- scripts/immortalwrt/patching.sh
+++ scripts/immortalwrt/patching.sh
@@ -4,11 +4,13 @@ shopt -s extglob
 ## Prepare
 # Add personal packages
 git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini.git ./package/luci-app-cpu-status-mini
+
 # Add Mediatek driver
-#git clone https://github.com/ALSe61/openwrt-r3p-mtk.git
-#rsync -av openwrt-r3p-mtk/target/ ./target && rsync -av --delete openwrt-r3p-mtk/package/mt/ ./package/mt
+# git clone https://github.com/ALSe61/openwrt-r3p-mtk.git
+# rsync -av openwrt-r3p-mtk/target/ ./target && rsync -av --delete openwrt-r3p-mtk/package/mt/ ./package/mt
+
 # Add turboacc packages
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 ## Update feeds
 # If the update is slow, use this command to swith from git.openwrt.org to github.com
@@ -21,10 +23,11 @@ sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqba
 
 ## Ending
 # My modificaions
-sed -i 's/192.168.1.1/192.168.41.11/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/AkiKiiro/g' package/base-files/files/bin/config_generate
-# Vermagic
-latest_release="$(curl -s https://api.github.com/repos/openwrt/openwrt/tags | grep -Eo "v23.05.+[0-9\.]" | head -n 1 | sed 's/v//g')"
-wget https://downloads.openwrt.org/releases/${latest_release}/targets/ramips/mt7621/packages/Packages.gz
+sed -i 's/192.168.1.1/192.168.41.1/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/AkiKiiro/g' package/base-files/files/bin/config_generate
+
+# Vermagic ImmortalWrt
+latest_version="$(curl -s https://api.github.com/repos/immortalwrt/immortalwrt/tags | grep -Eo "v23.05.+[0-9\.]" | head -n 1 | sed 's/v//g')"
+wget https://downloads.immortalwrt.org/releases/${latest_version}/targets/ramips/mt7621/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
