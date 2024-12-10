fb3ada4e4e0a2a59b7204ab53965acd7c1c8485a
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2024-12-10 14:30:51 +0700

    Update test build

--- scripts/only_test.sh
+++ scripts/only_test.sh
@@ -1,47 +1,34 @@
 #!/bin/bash
 shopt -s extglob
 
-## Add package repo
-echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
+## Prepare
+# Add personal packages
+git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini.git package/luci-app-cpu-status-mini
+git clone https://github.com/fichenx/openwrt-package package/openwrt-package
+
+# Add Mediatek driver
+git clone https://github.com/ALSe61/openwrt-r3p-mtk.git
+rsync -av openwrt-r3p-mtk/target/ ./target && rsync -av --delete openwrt-r3p-mtk/package/mt/ ./package/mt
+
+# Add turboacc packages
+ curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 ## Update feeds
+# If the update is slow, use this command to swith from git.openwrt.org to github.com
+# sed -i -E 's;git.openwrt.org/(feed|project);github.com/openwrt;' feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-################## Theme Modification ##########################
-sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-argon/root/etc/uci-defaults/30_luci-theme-argon
-sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-argon-mod/root/etc/uci-defaults/90_luci-theme-argon
-sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-material/root/etc/uci-defaults/30_luci-theme-material
-sed -i 's|set luci.main.mediaurlbase|#set luci.main.mediaurlbase|g' feeds/luci/themes/luci-theme-netgear/root/etc/uci-defaults/30_luci-theme-netgear
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-
-sed -i 's/luci-theme-argon/luci-theme-design/g' feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-argon/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
-sed -i 's/luci-theme-argon/luci-theme-design/g' feeds/luci/collections/luci-nginx/Makefile
-sed -i 's/luci-theme-argon/luci-theme-design/g' feeds/luci/collections/luci-ssl-nginx/Makefile
-
-
-################## Fix Error Compilation ##########################
-
-#Fix elfutils error
-git_svn master https://github.com/coolsnowwolf/lede package/libs/elfutils
-
-#Fix openssh error
-rm -rf feeds/packages/net/openssh
-git_sparse_clone master https://github.com/coolsnowwolf/packages net/openssh && mv -n openssh feeds/packages/net/openssh
-
-#Fix autocore error
-sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(arm||aarch64) \\/g' package/lean/autocore/Makefile
-
-#Fix zlib error
-git_sparse_clone main https://github.com/openwrt/openwrt package/libs/zlib && mv -n zlib package/libs/zlib
-sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.3.1/g' tools/zlib/Makefile
-sed -i 's/PKG_HASH:=.*/PKG_HASH:=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23/g' tools/zlib/Makefile
+## Patching
+# Irqbalance
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 
 ## Ending
 # My modificaions
 sed -i 's/192.168.1.1/192.168.41.1/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/NewifiD2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/AkiKiiro/g' package/base-files/files/bin/config_generate
+
+# Vermagic ImmortalWrt
+latest_version="$(curl -s https://api.github.com/repos/openwrt/openwrt/tags | grep -Eo "v23.05.+[0-9\.]" | head -n 1 | sed 's/v//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/ramips/mt7621/packages/Packages.gz
+zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
+sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
