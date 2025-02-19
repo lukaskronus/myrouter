52ebde58db078be8634b8e51d800cd3d80db65c9
lukaskronus <79519320+lukaskronus@users.noreply.github.com>
2025-02-19 18:26:03 +0700

    Update to v24.10.0

--- scripts/modification.sh
+++ scripts/modification.sh
@@ -3,15 +3,15 @@ shopt -s extglob
 
 ## Prepare
 # Add personal packages
-git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini.git package/luci-app-cpu-status-mini
-git clone https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
 #git clone https://github.com/NagaseKouichi/luci-app-dnsproxy.git ./package/luci-app-dnsproxy
 #git clone -b luci2 https://github.com/lukaskronus/luci-proto-batman-adv.git ./package/luci-proto-batman-adv
-#git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini.git ./package/luci-app-cpu-status-mini
+git clone https://github.com/gSpotx2f/luci-app-cpu-status.git ./package/luci-app-cpu-status
+git clone https://github.com/gSpotx2f/luci-app-temp-status.git ./package/luci-app-temp-status
+git clone https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
 
 # Add Mediatek driver
-git clone https://github.com/ALSe61/openwrt-r3p-mtk.git
-rsync -av openwrt-r3p-mtk/target/ ./target && rsync -av --delete openwrt-r3p-mtk/package/mt/ ./package/mt
+# git clone https://github.com/ALSe61/openwrt-r3p-mtk.git
+# rsync -av openwrt-r3p-mtk/target/ ./target && rsync -av --delete openwrt-r3p-mtk/package/mt/ ./package/mt
 
 # Add turboacc packages
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
